8a80a6788cdeeb55e515c87e0e1b940a4511552f
Aryan vallabhbhai <adv230003@utdallas.edu>
2026-05-21 00:26:15 +0000

    added grid world + basic training

--- include/environment.hpp
+++ include/environment.hpp
@@ -1,9 +1,31 @@
 #pragma once
 #include <string>
-
+#include <vector>
+struct Position {
+    int x,y;
+    bool operator==(const Position& other) const {
+        return x == other.x && y == other.y;
+    }
+};
+struct StepResult{
+    std::string next_state;
+    double reward;
+    bool done;
+};
 class Environment {
+private:
+    int grid_size;
+    Position start_pos;
+    Position goal_pos;
+    std::vector<Position> walls;
+    Position agent_pos;
+
+    std::string positionToString(const Position& pos);
 public:
+    Environment();
+    std::string reset();
     std::string getState();
+    StepResult step(const std::string& action);
     double getReward(const std::string& state, const std::string& action);
     std::string nextState(const std::string& state, const std::string& action);
 };
--- src/environment.cpp
+++ src/environment.cpp
@@ -1,7 +1,30 @@
 #include "environment.hpp"
+Environment::Environment() {
+    grid_size = 5;
+    start_pos = {0, 0};
+    goal_pos = {4, 4};
+    walls = {{1, 1}, {2, 2}, {3, 3}};
+    agent_pos = start_pos;
+}
+std::string Environment::positionToString(const Position& pos) {
+    return std::to_string(pos.x) + "," + std::to_string(pos.y);
+}
+std::string Environment::reset() {
+    agent_pos = start_pos;
+    return getState();
+}
 
 std::string Environment::getState() {
-    return "default_state";
+    return positionToString(agent_pos);
+}
+
+
+StepResult Environment::step(const std::string& action) {
+   Position next_pos = agent_pos;
+   
+   if(action == "UP") next_pos.y -=1;
+   else if (action == "RIGHT") next_pos.x +=1;
+   else
 }
 
 double Environment::getReward(const std::string& state, const std::string& action) {
